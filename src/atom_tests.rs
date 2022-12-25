1dbba9c078a7553022700d006bf53d9630599f3b
David L. Inman <davidlevere@gmail.com>
2022-12-24 23:45:27 -0500

    inc prog

--- src/atom_tests.rs
+++ src/atom_tests.rs
@@ -1,4 +1,5 @@
 
+static TEST : u8 = 0xFF;
 
 #[cfg(test)]
 mod test {
@@ -6,11 +7,60 @@ mod test {
 
     #[test]
     fn execute_statements_should_execute() {
-        let x = 0;
+        let _x = 0;
 
         let mut items = vec![];
-        atom!(x => { items.push(1); }; { items.push(2); } => { items.push(3); });
+        atom!(_x => { items.push(1); }; { items.push(2); } => { items.push(3); });
 
         assert_eq!(items, [1, 2, 3]);
     }
+
+    #[test]
+    fn execute_statements_should_be_visible_to_resolution() {
+        let _x = 0;
+
+        let mut ret = 0;
+        atom!(_x => { let y = 1; }; { let z = 2; } => { ret = z + y; });
+
+        assert_eq!(ret, 3);
+    }
+
+    #[test]
+    fn execute_statements_should_be_visible_to_subsequent_execute_statements() {
+        let _x = 0;
+
+        let mut ret = 0;
+        atom!(_x => { let y = 1; }; { let z = y + 1; } => { ret = z + y; });
+
+        assert_eq!(ret, 3);
+    }
+
+    #[test]
+    fn pattern_statements_should_pattern_match() {
+        let x = 1;
+
+        let mut ret = 0;
+        atom!(x => [ y ] => { ret = y; });
+
+        assert_eq!(ret, 1);
+    }
+
+    #[test]
+    fn pattern_statements_should_pattern_match_with_condition() {
+        let x = 1;
+
+        let mut ret = 0;
+        atom!(x => [ y if y == 1 ] => { ret = y; });
+
+        assert_eq!(ret, 1);
+    }
+
+    #[test]
+    fn initial_statement_should_work_with_namespace() {
+        let mut ret = 0;
+        atom!(crate::atom_tests::TEST => [ y if y != 0 ] => { ret = y; });
+
+        assert_eq!(ret, crate::atom_tests::TEST);
+    }
+
 }
